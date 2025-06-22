76835774228e420441cf630e8480876601894dba
Demo Outlook <sudhanshus9530@gmail.com>
2025-06-23 01:07:41 +0530

    added vertical image text and used it in catrgories on home screen

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
+
+import 'package:t_store/common/widgets/texts/section_heading.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:t_store/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:t_store/utils/constants/colors.dart';
+
+import 'package:t_store/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,10 +24,32 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   /// App Bar
                   const HomeAppBar(),
+                  const SizedBox(height: TSizes.spaceBtwSections),
 
                   /// Search Bar
+                  const TSearchContainer(text: 'Search in Store'),
+                  const SizedBox(height: TSizes.spaceBtwSections),
 
                   ///Categories
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        //Heading
+                        TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                          textColor: TColors.white,
+                        ),
+                        const SizedBox(height: TSizes.spaceBtwItems),
+
+                        // Horizontal Scrollable Categories
+                        // vertical height is not fixed in list view as
+                        THomeCategories(),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
