9bf2f2f607d14e3f780b9ba962cc9f92d683d8a2
Demo Outlook <sudhanshus9530@gmail.com>
2025-06-26 17:37:32 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/app.dart';
+import 'package:t_store/firebase_options.dart';
 
-void main() {
+/// Entry point of our application
+Future<void> main() async {
+  // Todo: Add Widgets Binding
+  // Todo: Init Local Storage
+  // Todo: Init Payment Methods
+  // Todo: Await Native Splash
+  // Todo: Initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then(
+    // whenever the firebase initialization is ready we are going to check our own firebase authentication
+    // it user is logged in or logout so we redirect the user to some other screen
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
+  // Todo : Initialize Authentication
+
   runApp(const App());
 }
